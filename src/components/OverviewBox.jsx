cbba48a0beb7f7a409761802db483b678e923242
el3raky <kameladel339@gmail.com>
2025-01-27 20:09:56 +0200

    Feat: created TourCard , overviewBox, reviewCard components

---
+++ src/components/OverviewBox.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const OverviewBox = ({ label, text, icon }) => {
+    return (
+        <div className="overview-box__detail">
+            <svg className="overview-box__icon">
+                <use xlinkHref={`/img/icons.svg#icon-${icon}`} />
+            </svg>
+            <span className="overview-box__label">{label}</span>
+            <span className="overview-box__text">{text}</span>
+        </div>
+    );
+};
+
+export default OverviewBox;
