0cfcfffbba165a426f62337ff3199e4ebe3d7eca
el3raky <kameladel339@gmail.com>
2025-02-10 02:50:52 +0200

    fetch tour from api and handle loading screen

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,32 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import { Outlet } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { RotateSpinner } from "react-spinners-kit"
+import { fetchTours } from "../rtk/features/TourSlice";
 
 const Layout = ({ title, alert, children }) => {
+
+    const dispatch = useDispatch()
+    let [loading, setLoading] = useState(true)
+
+
+    useEffect(() => {
+        dispatch(fetchTours()).then(() => setLoading(false))
+    }, [])
+
+    if (loading) {
+        return (
+            <div className='vh-100 d-flex justify-content-center align-items-center'>
+                <RotateSpinner size={100} color="#55c57a" loading={loading} />
+            </div>
+        )
+    }
+    
     return (
         <div data-alert={alert || ""}>
-            {/* <head>
-                <title>Natours</title>
-                <meta charSet="UTF-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <link
-                    href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700"
-                    rel="stylesheet"
-                />
-                <link rel="stylesheet" href="/css/style.css" />
-                <link rel="shortcut icon" type="image/png" href="/img/favicon.png" />
-                <link
-                    href="https://api.mapbox.com/mapbox-gl-js/v0.50.0/mapbox-gl.css"
-                    rel="stylesheet"
-                />
-                <script src="https://js.stripe.com/v3/"></script>
-                <script src="/js/bundle.js"></script>
-            </head> */}
-            <Header />
-            <Outlet />
-            {/* <main>{children}</main> */}
-            <Footer />
+            <>
+                <Header />
+                <Outlet />
+                <Footer />
+            </>
         </div>
     );
 };
