6df45aeb3adb9e4eb663bf1b928c3154583937fa
el3raky <kameladel339@gmail.com>
2025-02-12 04:00:40 +0200

    feat: imported authSlice

--- src/rtk/Store.jsx
+++ src/rtk/Store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import toursReducer from "./features/TourSlice.jsx";
+import authSlice from './features/AuthSlice';
 
 const store = configureStore({
     reducer: {
         tours: toursReducer,
+        auth: authSlice
     },
 });
 
